484646bb89bf9704811688e93279d848c7acee22
Devansh Chauhan <cdevansh631@gmail.com>
2026-01-17 18:59:12 +0530

    Adding my first numpy project

---
+++ Numpy-Project-[-1-].py
@@ -0,0 +1,16 @@
+#Sales Analysis System.
+import numpy as np
+Prices=[20,93,20,59,65,34,23,50]
+Sales=[4,4,4,5,6,6,9,3]
+Arr=np.array([Prices,Sales])
+Revenue=np.prod(Arr,axis=0)
+C=np.max(Sales)
+Index=np.argmax(Sales)
+print("Total Sales of Each product :",Revenue)
+print("Total Revenue :",np.sum(Revenue))
+print("Highest Sales :",C,"Having index ",Index)
+
+
+
+
+
